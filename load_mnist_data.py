6f4d345bc7786eb704ceaabe3a3580a5feca3c86
Pål Karlsrud <paulkarlsrud@gmail.com>
2016-10-22 01:43:36 +0200

    Make the magic number have a default value, as it is kinda silly to have to specify this manually.

--- load_mnist_data.py
+++ load_mnist_data.py
@@ -22,7 +22,7 @@ Source: http://yann.lecun.com/exdb/mnist/
 """
 
 IMG_MAGIC_NUM = 2051
-def parse_mnist_imgs(expected_magic_number, path_to_imgs):
+def parse_mnist_imgs(path_to_imgs, expected_magic_number=IMG_MAGIC_NUM):
     with open(path_to_imgs, 'rb') as binary_img_file:
         # Obtain info from the header
         read_magic_num, n_images, n_rows, n_cols = struct.unpack(">IIII", binary_img_file.read(16))
@@ -51,7 +51,7 @@ Source: http://yann.lecun.com/exdb/mnist/
 """
 
 LABELS_MAGIC_NUM = 2049
-def parse_mnist_labels(expected_magic_number, path_to_labels):
+def parse_mnist_labels(path_to_labels, expected_magic_number=LABELS_MAGIC_NUM):
     with open(path_to_labels, 'rb') as binary_labels_file:
         read_magic_num, n_labes = struct.unpack(">II", binary_labels_file.read(8))
 
