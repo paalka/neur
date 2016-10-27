8f0c3c97787f93c394a7b650fc56e34adbfb4f06
Pål Karlsrud <paulkarlsrud@gmail.com>
2016-10-27 14:17:52 +0200

    Convert some more magic values into constants with names that indicate what they are.

--- run_mnist.py
+++ run_mnist.py
@@ -6,22 +6,25 @@ from layers import LogisticLayer, SoftmaxOutputLayer, LinearLayer
 from utils import get_accuracy, convert_to_one_hot
 from NeuralNet import NeuralNet
 
-hidden_layer_1_neurons = 100
-hidden_layer_2_neurons = 100
-
-layer_1 = (LinearLayer(28 * 28, hidden_layer_1_neurons), LogisticLayer())
-layer_2 = (LinearLayer(hidden_layer_1_neurons, hidden_layer_2_neurons), LogisticLayer())
-layer_3 = (LinearLayer(hidden_layer_2_neurons, 10), SoftmaxOutputLayer())
-
-net = NeuralNet(layer_1, layer_2, layer_3)
-
 normalization_constant = 255.0 # 255 is the highest value a pixel in the MNIST dataset can have.
 mnist_n_possible_values = 10
+mnist_img_width = 28 # in pixels
+mnist_img_height = 28 # in pixels
 training_mnist_imgs = load_mnist_imgs("data/train-images-idx3-ubyte") / normalization_constant
 training_mnist_labels = convert_to_one_hot(load_mnist_labels("data/train-labels-idx1-ubyte"), mnist_n_possible_values)
 
 test_mnist_imgs = load_mnist_imgs("data/t10k-images-idx3-ubyte") / normalization_constant
 test_mnist_labels = convert_to_one_hot(load_mnist_labels("data/t10k-labels-idx1-ubyte"), mnist_n_possible_values)
+
+
+hidden_layer_1_neurons = 100
+hidden_layer_2_neurons = 100
+
+layer_1 = (LinearLayer(mnist_img_width * mnist_img_height, hidden_layer_1_neurons), LogisticLayer())
+layer_2 = (LinearLayer(hidden_layer_1_neurons, hidden_layer_2_neurons), LogisticLayer())
+layer_3 = (LinearLayer(hidden_layer_2_neurons, mnist_n_possible_values), SoftmaxOutputLayer())
+
+net = NeuralNet(layer_1, layer_2, layer_3)
 
 X_train, X_validation, T_train, T_validation = train_test_split(training_mnist_imgs, training_mnist_labels, test_size=0.2)
 
