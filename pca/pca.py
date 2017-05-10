5f3a0fc3a9569bca883e30b68e43f3be78264ab9
Pål Karlsrud <paulkarlsrud@gmail.com>
2017-05-10 11:03:11 +0200

    Move the initilisation of the means closer to where it is first used.

--- pca/pca.py
+++ pca/pca.py
@@ -11,13 +11,13 @@ class PCA(preprocessing.FunctionTransformer):
         return self
 
     def transform(self, X):
-        means = np.mean(all_samples, axis=1)
-
         cov_matrix = np.cov(all_samples)
         eig_val, eig_vec = np.linalg.eig(cov_matrix)
 
         eig_pairs = [(np.abs(eig_val[i]), eig_vec[:,i]) for i in range(len(eig_val))]
         eig_pairs.sort(key=lambda x: x[0], reverse=True)
 
-        matrix_w = np.hstack([pair[1].reshape(pair[1].shape[0], 1) for pair in eig_pairs[:self.n_components]])
-        return matrix_w.T.dot(all_samples - means[:,np.newaxis]).T
+        w = np.hstack([pair[1].reshape(pair[1].shape[0], 1) for pair in eig_pairs[:self.n_components]])
+
+        sample_means = np.mean(all_samples, axis=1)
+        return w.T.dot(all_samples - sample_means[:,np.newaxis]).T
