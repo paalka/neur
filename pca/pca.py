025128ba48c863424abacb571a3f02a0fc408174
Pål Karlsrud <paulkarlsrud@gmail.com>
2017-05-10 11:21:04 +0200

    Refer to the correct variable when using the sample data.

--- pca/pca.py
+++ pca/pca.py
@@ -13,7 +13,7 @@ class PCA(preprocessing.FunctionTransformer):
     def transform(self, X):
         # Use the eigenvectors of the covariance matrix to determine the
         # major directions of variation in the data.
-        cov_matrix = np.cov(all_samples)
+        cov_matrix = np.cov(X)
         eig_val, eig_vec = np.linalg.eig(cov_matrix)
 
         eig_pairs = [(np.abs(eig_val[i]), eig_vec[:,i]) for i in range(len(eig_val))]
@@ -22,5 +22,5 @@ class PCA(preprocessing.FunctionTransformer):
         # Pick the 'n_components' vectors with the greatest variation.
         w = np.hstack([pair[1][:,np.newaxis] for pair in eig_pairs[:self.n_components]])
 
-        sample_means = np.mean(all_samples, axis=1)
-        return w.T.dot(all_samples - sample_means[:,np.newaxis]).T
+        sample_means = np.mean(X, axis=1)
+        return w.T.dot(X - sample_means[:,np.newaxis]).T
